0b59cd2a622f0f35c4f2b480a720ea25239131c9
Divya112989 <maddimadugud@mail.sacredheart.edu>
2025-09-26 17:29:26 -0500

    yes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,13 @@
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
+  resource_provider_registrations = "none"
+
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
+
+
 
 
