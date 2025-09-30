c8e964402cfd039b40edf41ebc10893e359a779b
Divya112989 <maddimadugud@mail.sacredheart.edu>
2025-09-29 23:09:51 -0500

    yes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 # NOTE: Variables below are unused. Authentication is done via environment variables (ARM_*).
-variable "client_id" {}
-variable "client_secret" {}
-variable "subscription_id" {}
-variable "tenant_id" {}
+#variable "client_id" {}
+#variable "client_secret" {}
+#variable "subscription_id" {}
+#variable "tenant_id" {}
 
