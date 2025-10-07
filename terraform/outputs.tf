aa56333f611c304ee197aaae09683edca6024c1f
Divya112989 <maddimadugud@mail.sacredheart.edu>
2025-10-06 21:07:43 -0500

    yes

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "dev_rg_name" {
-  value = azurerm_resource_group.rg_dev.name
-}
-
-output "test_rg_name" {
-  value = azurerm_resource_group.rg_test.name
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,4 +2,5 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "subscription_id" {}
 variable "tenant_id" {}
+ 
 
